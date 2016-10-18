20bb15d354162058120b02448fc4b151db467346
Thijs Kinkhorst <thijs@kinkhorst.com>
2016-10-18 17:16:47 +0200

    Change examples for new syntax.

--- examples/test.py
+++ examples/test.py
@@ -10,27 +10,27 @@ foo = ServiceRegistry(username='pino',password='1en1em1en1e',baseurl='https://se
 #foo.list(entityid='http://ste.test.iqualify.nl/SAML2')
 
 print("\nFetching basic record\n\n")
-basic = foo.getByEntityId('bazo:test',)
+basic = foo.get('bazo:test',)
 pprint(basic)
 id = basic['id']
 print("Found entity with id=%u\n" % id)
 
 print("\nFetching full record\n\n")
-extended = foo.getById(id)
+extended = foo.get(id)
 extended['metadata']['OrganizationName'] = {"en": "Bas is erg tof"}
 
 pprint(extended)
 
 print("\nUpdating record\n\n")
-foo.updateById(id,extended)
+foo.update(id,extended)
 
 print("\nAdding record\n\n")
 extended['name'] = 'bazo:test3'
 newentity = foo.add(extended)
 
 print("\nFetching full new record\n\n")
-extended2 = foo.getById(newentity['id'])
+extended2 = foo.get(newentity['id'])
 pprint(extended2)
 
 print("\nRemoving entity\n\n")
-foo.deleteById(newentity['id'])
+foo.delete(newentity['id'])
